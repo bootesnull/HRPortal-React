80e4499f99883f19bbaf807583c32f31d899d47e
gurjit <gurjits@bootesnull.com>
2022-07-12 19:06:13 +0530

    event module list and delete layout + add event

--- src/Component/EventsTable/EventsTable.js
+++ src/Component/EventsTable/EventsTable.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { eventTypelist,  eventTypeCreate, eventTypeView, eventTypeEdit } from "../../reducers/eventsReducer";
+import { eventTypelist,  eventTypeCreate, eventTypeView, eventTypeEdit, eventsCreate, eventsList, eventsDelete } from "../../reducers/eventsReducer";
 import TableModal from "../utils/TableModal";
 import { toast } from "react-toastify";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const EventsTable = () => {
 
     const dispatch = useDispatch();
-    const eventList = useSelector((state)=> state.Events)
+   // const eventList = useSelector((state)=> state.Events)
     const [eventTbData, setEventTbData] = useState([{}]);
     const [showBasicModal, setShowBasicModal] = useState(false);
     const [modalTitle, setModalTitle] = useState('');
     const [flag, setFlag] = useState();
     const eventRendering = useSelector((state)=> state.Events)
 
-    const [createEvent, setCreateEvent] = useState({
+    const [createEventType, setCreateEventType] = useState({
         name:"",
     });
 
@@ -41,22 +43,16 @@ const EventsTable = () => {
             setEventTbData([...data])
         }
         eventTypelist(callback)
-    },[eventList])
+    },[eventRendering])
 
-    // Delete handler
-    // const handleDelete = (id) => {
-    //     // const newData = announcementTbData.filter((item) => item.id !== id);
-    //     // setPermissionTbData(newData);
-    //     //console.log(id)
-    //     dispatch(eventDelete(id))
-    // };
+ 
 
     const showModal = () => {
         setShowBasicModal(true);
     }
 
     const handleAddChange = (e) => {
-        setCreateEvent((state)=> {
+        setCreateEventType((state)=> {
            return {...state, [e.target.name]: e.target.value }
         })
         console.log(e.target.name, e.target.value)
@@ -65,7 +61,7 @@ const EventsTable = () => {
     // Create event handle
     const handleAddEvent = (e) => {
         e.preventDefault();
-        dispatch(eventTypeCreate(createEvent))
+        dispatch(eventTypeCreate(createEventType))
         setShowBasicModal(false)
     }    
     
@@ -97,6 +93,54 @@ const EventsTable = () => {
         setActiveTab("tab2");
     };
 
+    const [eventlistData, setEventlistData] = useState([{}]);
+
+    const [addEvent, setAddEvent] = useState({
+        title:'', 
+        event_type_id:'', 
+        date:new Date(),
+        description:'',
+        is_holiday:'', 
+        holiday_from_date: new Date(), 
+        holiday_to_date: new Date(),
+        banner:'',
+    });
+    console.log(addEvent.holiday_from_date)
+
+    useEffect(()=> {
+        const callback = (data) => {
+            setEventlistData([...data])
+        }
+        eventsList(callback);
+    },[eventRendering])
+
+
+
+    const handleEventAddChange = (e) => {
+        setAddEvent((prevState) => {
+            return {...prevState, [e.target.name]:e.target.value }
+        })
+        //console.log(e.target.name,e.target.value)
+    }
+
+    const handleEventSave = (e) => {
+        e.preventDefault();
+        dispatch(eventsCreate(addEvent))
+        console.log(addEvent)
+    }
+
+       // Delete handler
+    const handleDelete = (id) => {
+        // const newData = announcementTbData.filter((item) => item.id !== id);
+        // setPermissionTbData(newData);
+        //console.log(id)
+        dispatch(eventsDelete(id))
+    };
+
+//    const [eventDate, setEventDate] = useState(new Date());
+//    const [eventFromDate, setEventFromDate] = useState(new Date());
+//    const [eventToDate, setEventToDate] = useState(new Date())
+
     return(
         <div>
 
@@ -108,45 +152,89 @@ const EventsTable = () => {
                 <div className="tab-content">
                     {(activeTab === "tab1" ? 
                         <div className="tab-pane fade show active">
-                            <h5 className="card-title"><b>Events List</b>
-                                    <button
-                                        className="btn btn-primary modal-btn"
-                                        onClick={()=> {
-                                            showModal()
-                                            setModalTitle( 'Create Event')
-                                            setFlag(false)
-                                        }}
-                                        >Add Event </button>
-                            </h5>
+                            <h5 className="card-title"><b>Events List</b></h5>
+
+
+                            <form className="row" onSubmit={handleEventSave} >
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Title</label>
+                                    <input type="text" className="form-control" placeholder='Title' name="title" onChange={handleEventAddChange} required="required" autoComplete="off" />
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Event Type</label>
+                                    <select className="form-select" name="event_type_id" onChange={handleEventAddChange}>
+                                    <option defaultValue>Select Event Type</option>
+                                    {eventTbData && eventTbData.map((eventType, id) => {return   <option key={id} value={eventType.id}>{eventType.name}</option>})}
+                                    </select>
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Date</label>
+                                    {/* <DatePicker name="date" className="form-control" selected={eventDate} onChange={(date) => setEventDate(date)} /> */}
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Description</label>
+                                    <input type="text" className="form-control" placeholder='Description' name="description" onChange={handleEventAddChange} required="required" autoComplete="off" />
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Is Holiday</label>
+                                    <input type="text" className="form-control" placeholder='is_holiday' name="is_holiday" onChange={handleEventAddChange} required="required" autoComplete="off" />
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Holiday From Date</label>
+                                    <DatePicker name="holiday_from_date" className="form-control"  selected={addEvent.holiday_from_date} onChange={(date) => handleEventAddChange(date)} />
+                                </div>
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Holiday To Date</label>
+                                    {/* <DatePicker name="holiday_to_date" className="form-control"  selected={eventToDate} onChange={(date) => setEventToDate(date)} /> */}
+                                </div> 
+                                <div className="col-lg-3 mb-3">
+                                    <label className="form-label">Banner</label>
+                                    <input type="file" className="form-control" name="banner" onChange={handleEventAddChange} required="required" autoComplete="off" />
+                                </div>
+
+                                <div className="col-lg-12 mb-3">
+                                    <button type="submit" className="btn btn-primary "  >Save</button>
+                                    <button type="button" className="btn btn-secondary mx-2" >Cancel</button>
+                                </div>
+                            </form>
+
 
                             <table className='table table-bordered'>
                                 <thead>
                                     <tr>
                                         <th>#ID</th>
-                                        <th>Name</th>
-                                        <th>Created at</th>
-                                        <th>Updated at</th>
+                                        <th>Title</th>
+                                        <th>Event Type Name</th>
+                                        <th>Descriptions</th>
+                                        <th>Is Holiday</th>
+                                        <th>From Date</th>
+                                        <th>To Date</th>
+                                        <th>Banner</th>
                                         <th>Status</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {eventTbData && eventTbData.map((events, index) => {
+                                    {eventlistData && eventlistData.map((events, index) => {
                                         return (
                                             <tr key={index}>
-                                                <td>{events.id}</td>
-                                                <td>{events.name}</td>
-                                                <td>{events?.created_at?.slice(0, 10)}</td>
-                                                <td>{events?.updated_at?.slice(0, 10)}</td>
+                                                <td>{events.events_id}</td>
+                                                <td>{events.events_title}</td>
+                                                <td>{events.event_type_name}</td>
+                                                <td>{events.events_descriptions}</td>
+                                                <td>{events.is_holiday}</td>
+                                                <td>{events?.holiday_from_date?.slice(0, 10)}</td>
+                                                <td>{events?.holiday_to_date?.slice(0, 10)}</td>
+                                                <td>{events.image_url}</td>
                                                 <td>{events.status}</td>
                                                 <td>
                                                     <button className="btn btn-secondary  btn-sm mx-1"
                                                                                                             
                                                     >Edit</button>
 
-                                                    {/* <button className="btn btn-secondary  btn-sm mx-1"
-                                                        onClick={()=> handleDelete(events.id)}
-                                                        >Delete</button> */}
+                                                    <button className="btn btn-secondary  btn-sm mx-1"
+                                                        onClick={()=> handleDelete(events.events_id)}
+                                                        >Delete</button>
                                                 </td>
                                             </tr>
                                         );
@@ -154,45 +242,7 @@ const EventsTable = () => {
                                 </tbody>
                             </table>
 
-                            
-                            <TableModal
-                                show={showBasicModal}
-                                cancelModal={setShowBasicModal}
-                                modalHeading={modalTitle}
 
-                                structure={(
-                                            <div>
-                                            {flag ? (<>
-                                                <form onSubmit={handleEventTypeEditSave} >
-                                                    <div className="mb-3">
-                                                        <label className="form-label">Edit Event Title</label>
-                                                        <input type="text" className="form-control"  value={editEventType.name} placeholder='Event Name' name="name" onChange={handleEditTypeChange} required="required" autoComplete="off" />
-                                                    </div>
-
-                                                    <div className="modal-footer">
-                                                        <button type="button" className="btn btn-secondary" onClick={(e) => setShowBasicModal(false)}  >Cancel</button>
-                                                        <button type="submit" className="btn btn-primary"  >Save</button>
-                                                    </div>
-                                                </form>
-                                            </>) : (
-                                                <>
-                                                    <form onSubmit={handleAddEvent} >
-                                                        <div className="mb-3">
-                                                            <label className="form-label">Event Title</label>
-                                                            <input type="text" className="form-control"  placeholder='Event Name' name="name" onChange={handleAddChange} required="required" autoComplete="off" />
-                                                        </div>
-
-                                                        <div className="modal-footer">
-                                                            <button type="button" className="btn btn-secondary" onClick={(e) => setShowBasicModal(false)}  >Cancel</button>
-                                                            <button type="submit" className="btn btn-primary"  >Save</button>
-                                                        </div>
-                                                    </form>
-                                                </>
-                                            ) }
-                                                
-                                            </div>    
-                                        )}
-                            />
                         </div>
                         : 
                         <div className="tab-pane fade show active">
@@ -219,26 +269,26 @@ const EventsTable = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {eventTbData && eventTbData.map((events, index) => {
+                                    {eventTbData && eventTbData.map((eventType, index) => {
                                         return (
                                             <tr key={index}>
-                                                <td>{events.id}</td>
-                                                <td>{events.name}</td>
-                                                <td>{events?.created_at?.slice(0, 10)}</td>
-                                                <td>{events?.updated_at?.slice(0, 10)}</td>
-                                                <td>{events.status}
+                                                <td>{eventType.id}</td>
+                                                <td>{eventType.name}</td>
+                                                <td>{eventType?.created_at?.slice(0, 10)}</td>
+                                                <td>{eventType?.updated_at?.slice(0, 10)}</td>
+                                                <td>{eventType.status}
                                                     {/* <input type="checkbox"
                                                             className='cm-toggle'
-                                                            checked={events.status}
+                                                            checked={eventType.status}
                                                             name="status"
-                                                            id={events.id}
-                                                            value={events.status}
-                                                            onChange={(e)=> handleUpdateStatus(e, events.id) }
+                                                            id={eventType.id}
+                                                            value={eventType.status}
+                                                            onChange={(e)=> handleUpdateStatus(e, eventType.id) }
                                                     />         */}
                                                 </td>
                                                 <td>
                                                     <button className="btn btn-secondary  btn-sm mx-1"
-                                                    onClick={()=> handleEventTypeEdit(events.id)}
+                                                    onClick={()=> handleEventTypeEdit(eventType.id)}
                                                         
                                                     >Edit</button>
 
--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -36,7 +36,7 @@ export const eventTypelist = async (callback) => {
 
 // Event Type create API
 export const eventTypeCreate = createAsyncThunk(
-    'events/event-create',
+    'events/event-type-create',
     async ( { name}, thunkAPI) => {
         try{
             const response = await fetch(
@@ -121,45 +121,124 @@ export const eventTypeEdit = createAsyncThunk(
 );  
 
 
-// Event Type Edit API
-export const eventCreate = createAsyncThunk(
-    'events/event-type-edit',
-    async ( {id, name}, thunkAPI) => {
+
+// Event List API
+export const eventsList = async(callback) => {
         try{
             const response = await fetch(
-                `${API_URL}/api/events/event_type/edit`,
+                `${API_URL}/api/events/events/list`,
                 {
-                    method: "PUT",
-                    headers : {
-                        Accept : "application/json",
+                    method: "GET",
+                    headers: {
                         "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            let data = await response.json();
+            callback(data.data)
+            console.log(data)
+        }catch(e){
+            console.log("error", e.response.data);
+        }
+    }
+
+
+
+    
+// Event Create API
+export const eventsCreate = createAsyncThunk(
+    'events/event-create',
+    async({title, event_type_id, date, description, is_holiday, holiday_from_date, holiday_to_date, banner}, thunkAPI) => {
+        try{
+            const response = await fetch(
+                `${API_URL}/api/events/events/create`,
+                {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
                     },
                     body: JSON.stringify({
-                        id:id,
-                        name: name,
+                        title, event_type_id, date, description, is_holiday, holiday_from_date, holiday_to_date, banner
                     })
                 }
             );
-            let data = response.json();
-           // console.log(data);
+            let data = await response.json();
+            console.log(data)
             return data;
-        }catch(e) {
-            console.log("error", e.response.data)
-            thunkAPI.rejectWithValue(e.response.data)
+        }catch(e){
+            console.log("error", e.response.data);
+            thunkAPI.rejectWithValue(e.response.data);
         }
     }
-); 
+);
+
+// Event  - view event by event id  API
+export const eventsViewId = createAsyncThunk(
+    'events/event-view-id',
+    async(id, thunkAPI) => {
+        try{
+            const response = await fetch(
+                `${API_URL}/api/events/events/view?id=${id}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            let data = await response.json();
+            console.log(data)
+            return data;
+        }catch(e){
+            console.log("error", e.response.data);
+            thunkAPI.rejectWithValue(e.response.data);
+        }
+    }
+);
+
+
+// Event Edit API
+export const eventsEdit = createAsyncThunk(
+    'events/event-edit',
+    async({event_id, title, event_type_id, date, description, is_holiday, holiday_from_date, holiday_to_date, banner}, thunkAPI) => {
+        try{
+            const response = await fetch(
+                `${API_URL}/api/events/events/edit`,
+                {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: JSON.stringify({
+                        event_id, title, event_type_id, date, description, is_holiday, holiday_from_date, holiday_to_date, banner
+                    })
+                }
+            );
+            let data = await response.json();
+            console.log(data)
+            return data;
+        }catch(e){
+            console.log("error", e.response.data);
+            thunkAPI.rejectWithValue(e.response.data);
+        }
+    }
+);
 
 // Event delete API
-export const eventDelete = createAsyncThunk(
+export const eventsDelete = createAsyncThunk(
     'events/event-delete',
     async(id, thunkAPI) => {
         try{
             const response = await fetch(
                 `${API_URL}/api/events/events/delete?id=${id}`,
                 {
-                    method: "PUT",
+                    method: "DELETE",
                     headers : {
                         Accepts: "application/json",
                         "Content-Type" : "application/json",
@@ -228,6 +307,58 @@ const eventsReducer = createSlice ({
             return {...action.payload}
         },
 
+
+
+
+        [eventsList.fulfilled] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsList.pending] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsList.rejected] : (state, action) => {
+            return {...action.payload}
+        },
+
+        [eventsCreate.fulfilled] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsCreate.pending] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsCreate.rejected] : (state, action) => {
+            return {...action.payload}
+        },
+
+        [eventsViewId.fulfilled] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsViewId.pending] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsViewId.rejected] : (state, action) => {
+            return {...action.payload}
+        },
+
+        [eventsEdit.fulfilled] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsEdit.pending] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsEdit.rejected] : (state, action) => {
+            return {...action.payload}
+        },
+
+        [eventsDelete.fulfilled] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsDelete.pending] : (state, action) => {
+            return {...action.payload}
+        },
+        [eventsDelete.rejected] : (state, action) => {
+            return {...action.payload}
+        },
         
     },
 
