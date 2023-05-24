26bc47edfcb7090566fa3dcfbc27d3709570dde3
Rohan Rana <rohanrana@bootesnull.com>
2023-05-24 18:52:12 +0530

    leave API with Month Filter Integration Completed

--- src/pages/AttendanceDetails/index.js
+++ src/pages/AttendanceDetails/index.js
@@ -5,10 +5,15 @@ import moment from "moment";
 import { API_URL } from "../../api";
 
 const AttendanceDetails = () => {
+
+    // useParams is used to get the URL Parameters from the routing URL.
     const { id } = useParams()
+
+    //States
     const [selectedDate, setSelectedDate] = useState("2023-05-23")
     const [attendanceLogs, setAttendanceLogs] = useState([])
 
+    // Function to call Attendance Detail API
     const attendanceDetailsAPI = async () => {
         const response = await axios.post(`${API_URL}/api/attendence/attendence-per-user?id=${id}`, {
             "date": selectedDate
@@ -31,7 +36,7 @@ const AttendanceDetails = () => {
                                 setSelectedDate(e.target.value)
                             }}></input>
                             <p style={{ marginLeft: "10px", marginBottom: "0" }}>Total Working Hours
-                                <span style={{ fontSize: "18px", marginLeft: "10px" }}><b>{`${attendanceLogs?.total_time.split(":")[0]}hours ${attendanceLogs?.total_time.split(":")[1]}minutes`}</b></span></p>
+                                <span style={{ fontSize: "18px", marginLeft: "10px" }}><b>{attendanceLogs?.total_time ? `${attendanceLogs?.total_time?.split(":")[0]}hours ${attendanceLogs?.total_time?.split(":")[1]}minutes` : `00hours 00minutes`}</b></span></p>
                         </div>
                         <div style={{ marginTop: "10px" }}>
                             <table className='table table-bordered'>
@@ -46,9 +51,9 @@ const AttendanceDetails = () => {
                                     {attendanceLogs?.session_data && attendanceLogs?.session_data.map((ele) => {
                                         return (
                                             <tr>
-                                                <td>{moment(ele.checkInTime).format("LTS")}</td>
-                                                <td>{moment(ele.checkOutTime).format("LTS") === "Invalid date" ? "00:00:00" : moment(ele.checkOutTime).format("LTS")}</td>
-                                                <td>{`${ele.session_time.split(":")[0]}hours${ele.session_time.split(":")[1]}minutes`}</td>
+                                                <td>{moment(ele?.checkInTime).format("LTS")}</td>
+                                                <td>{moment(ele?.checkOutTime).format("LTS") === "Invalid date" ? "00:00:00" : moment(ele.checkOutTime).format("LTS")}</td>
+                                                <td>{`${ele?.session_time?.split(":")[0]}hours${ele?.session_time?.split(":")[1]}minutes`}</td>
                                             </tr>
                                         )
                                     })}
--- src/pages/LeaveDetails/index.js
+++ src/pages/LeaveDetails/index.js
@@ -1,28 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import dayjs from "dayjs";
+import { DatePicker, Input } from "antd";
+import "antd/dist/antd.css";
 import { API_URL } from "../../api";
 
 const LeaveDetails = () => {
 
+    // useParams is used to get the URL Parameters from the routing URL.
     const { id } = useParams()
+    const monthArray = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    //States
     const [employeeLeaveData, setEmployeeLeaveData] = useState([]);
     const [currentAndLastMonth, setCurrentAndLastMonth] = useState([]);
+    const [month, setMonth] = useState("2023-05")
+    const [monthName, setMonthName] = useState("")
 
+    // Function to call pending leaves count API
     const pendingLeaveCount = async () => {
         const response = await axios.get(`${API_URL}/api/leaves/user-pending-leaves?id=${id}`)
         setEmployeeLeaveData(response?.data?.data)
     }
 
+    //Function to call CurrentAndLastMonthLeaves API
     const currentAndLastMonthLeaveDetails = async () => {
-        const response = await axios.get(`${API_URL}/api/leaves/leave-taken-last-currnet-month?id=${id}`)
+        const response = await axios.get(`${API_URL}/api/leaves/leave-taken-last-currnet-month?id=${id}&month=${month}`)
         setCurrentAndLastMonth(response.data.data[0])
+    }
+
+    //Function used to handle the change of month
+    const monthOnChange = (date, dateString) => {
+        setMonth(dateString)
     }
 
     useEffect(() => {
         pendingLeaveCount()
-        currentAndLastMonthLeaveDetails()
     }, [])
+
+    useEffect(() => {
+        if (month.length) {
+            currentAndLastMonthLeaveDetails()
+            setMonthName(monthArray[new Date(month).getMonth()])
+        }
+    }, [month])
 
     return (
         <div>
@@ -30,7 +53,7 @@ const LeaveDetails = () => {
                 <div className="card">
                     <div className="card-body" >
                         <h5 className="card-title"><b>Leave Count Details</b></h5>
-                        <div>
+                        <div style={{ marginTop: "20px" }}>
                             <table className='table table-bordered'>
                                 <thead>
                                     <tr>
@@ -61,7 +84,13 @@ const LeaveDetails = () => {
                 <div className="card">
                     <div className="card-body" >
                         <h5 className="card-title"><b>Current Month Leave Details</b></h5>
-                        <div>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <span style={{ marginRight: "5px", fontSize: "16px" }}>Select Month</span>
+                            <DatePicker defaultValue={dayjs("2023-05", "YYYY-MM")} format={"YYYY-MM"} onChange={monthOnChange} picker="month" />
+                            <span style={{ marginLeft: "20px", fontSize: "16px" }}>Month</span>
+                            <Input disabled style={{ marginLeft: "5px", width: 200 }} value={monthName}/>
+                        </div>
+                        <div style={{ marginTop: "15px" }}>
                             <table className='table table-bordered'>
                                 <thead>
                                     <tr>
@@ -85,7 +114,7 @@ const LeaveDetails = () => {
                     </div>
                 </div>
             </div>
-            <div style={{ marginTop: "20px" }} className='container-fluid'>
+            <div style={{ marginTop: "15px" }} className='container-fluid'>
                 <div className="card">
                     <div className="card-body" >
                         <h5 className="card-title"><b>Last Month Leave Details</b></h5>
